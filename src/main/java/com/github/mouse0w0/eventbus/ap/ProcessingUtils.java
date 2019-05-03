ed2eb127871d59b463a45ab8fbbf180a0d8b5123
Mouse <mouse0w0@qq.com>
2019-05-04 01:07:57 +0800

    Add ListenerProcessor

---
+++ src/main/java/com/github/mouse0w0/eventbus/ap/ProcessingUtils.java
@@ -0,0 +1,28 @@
+package com.github.mouse0w0.eventbus.ap;
+
+import javax.lang.model.element.Element;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.Name;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeMirror;
+import java.util.List;
+
+public class ProcessingUtils {
+
+    public static boolean hasModifier(Element element, Modifier modifier) {
+        return element.getModifiers().stream().anyMatch(m -> modifier == m);
+    }
+
+    public static Name getQualifiedName(TypeMirror typeMirror) {
+        return ((TypeElement) ((DeclaredType) typeMirror).asElement()).getQualifiedName();
+    }
+
+    public static List<? extends TypeMirror> getInterfaces(TypeMirror typeMirror) {
+        return ((TypeElement) ((DeclaredType) typeMirror).asElement()).getInterfaces();
+    }
+
+    public static TypeMirror getSuperclass(TypeMirror typeMirror) {
+        return ((TypeElement) ((DeclaredType) typeMirror).asElement()).getSuperclass();
+    }
+}
