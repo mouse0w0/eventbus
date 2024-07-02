72676b18b2954f5fc661577920c71ab835b6ab83
Mouse <mouse0w0@qq.com>
2024-07-02 15:26:32 +0800

    feat: remove ConsumerListenerInvoker eventType field

--- src/main/java/com/github/mouse0w0/eventbus/ConsumerListenerInvoker.java
+++ src/main/java/com/github/mouse0w0/eventbus/ConsumerListenerInvoker.java
@@ -3,16 +3,15 @@ package com.github.mouse0w0.eventbus;
 import java.util.function.Consumer;
 
 public class ConsumerListenerInvoker<T extends Event> implements ListenerInvoker {
-    private final Class<T> eventType;
     private final Consumer<T> consumer;
 
-    public ConsumerListenerInvoker(Class<T> eventType, Consumer<T> consumer) {
-        this.eventType = eventType;
+    public ConsumerListenerInvoker(Consumer<T> consumer) {
         this.consumer = consumer;
     }
 
     @Override
+    @SuppressWarnings("unchecked")
     public void invoke(Event event) throws Throwable {
-        consumer.accept(eventType.cast(event));
+        consumer.accept((T) event);
     }
 }
