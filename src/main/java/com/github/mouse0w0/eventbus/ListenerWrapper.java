9931c953075ba5aae5653c823e341642b841994f
Mouse <mouse0w0@qq.com>
2024-07-02 17:12:35 +0800

    perf: optimize ListenerWrapper

--- src/main/java/com/github/mouse0w0/eventbus/ListenerWrapper.java
+++ src/main/java/com/github/mouse0w0/eventbus/ListenerWrapper.java
@@ -1,42 +1,25 @@
 package com.github.mouse0w0.eventbus;
 
 import java.lang.reflect.Type;
-import java.util.function.Predicate;
 
 final class ListenerWrapper {
     public static final ListenerWrapper[] EMPTY_ARRAY = new ListenerWrapper[0];
 
-    private static final Predicate<Event> ALWAYS_TRUE = event -> true;
-    private static final Predicate<Event> NOT_CANCELLED = ListenerWrapper::notCancelled;
+    private static final int RECEIVE_CANCELLED = 1;
+    private static final int IS_GENERIC = 2;
 
     private final Class<?> eventType;
     private final Type genericType;
     private final Order order;
     private final ListenerInvoker invoker;
-    private final Predicate<Event> filter;
+    private final int filterFlags;
 
     public ListenerWrapper(Class<?> eventType, Type genericType, Order order, boolean receiveCancelled, ListenerInvoker invoker) {
         this.eventType = eventType;
         this.genericType = genericType;
         this.order = order;
         this.invoker = invoker;
-        this.filter = createFilter(receiveCancelled, genericType != null);
-    }
-
-    private Predicate<Event> createFilter(boolean receiveCancelled, boolean isGeneric) {
-        if (receiveCancelled) {
-            if (isGeneric) {
-                return this::checkGeneric;
-            } else {
-                return ALWAYS_TRUE;
-            }
-        } else {
-            if (isGeneric) {
-                return event -> notCancelled(event) && checkGeneric(event);
-            } else {
-                return NOT_CANCELLED;
-            }
-        }
+        this.filterFlags = (receiveCancelled ? RECEIVE_CANCELLED : 0) | (genericType != null ? IS_GENERIC : 0);
     }
 
     public Class<?> getEventType() {
@@ -48,16 +31,30 @@ final class ListenerWrapper {
     }
 
     public void post(Event event) throws Throwable {
-        if (filter.test(event)) {
+        if (filter(event)) {
             invoker.invoke(event);
         }
     }
 
-    private static boolean notCancelled(Event event) {
+    private boolean filter(Event event) {
+        switch (filterFlags) {
+            case 0:
+            default:
+                return isNotCancelled(event);
+            case RECEIVE_CANCELLED:
+                return true;
+            case IS_GENERIC:
+                return isNotCancelled(event) && isSameGenericType(event);
+            case RECEIVE_CANCELLED | IS_GENERIC:
+                return isSameGenericType(event);
+        }
+    }
+
+    private static boolean isNotCancelled(Event event) {
         return !event.isCancellable() || !((Cancellable) event).isCancelled();
     }
 
-    private boolean checkGeneric(Event event) {
+    private boolean isSameGenericType(Event event) {
         return ((GenericEvent<?>) event).getGenericType() == genericType;
     }
 }
