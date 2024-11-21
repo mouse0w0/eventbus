d62acf97c15d9ba7ee92da96ceeed39f21ddcc65
Mouse <mouse0w0@qq.com>
2024-11-21 16:42:56 +0800

    style: reformat code

--- src/main/java/com/github/mouse0w0/eventbus/Event.java
+++ src/main/java/com/github/mouse0w0/eventbus/Event.java
@@ -4,7 +4,7 @@ package com.github.mouse0w0.eventbus;
  * Every event class should implement this interface.
  */
 public interface Event {
-	default boolean isCancellable() {
+    default boolean isCancellable() {
         return this instanceof Cancellable;
-	}
+    }
 }
